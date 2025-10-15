66aa8ade684e2b7dd24d04f127bd1a04b4c5d116
lolasudo <etosdelaljamal@gmail.com>
2025-10-15 22:12:48 +0300

    Добавлен фронт React в папку frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,8 @@
-import React from 'react';
-import Home from './pages/Home';
-
+import React from "react";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
